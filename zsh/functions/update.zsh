c38047732bec6180832b4f5fae570d39dec307cb
oljfte <oljfte@gmail.com>
2020-12-25 23:56:34 +0900

    Rename update functions

--- zsh/functions/update.zsh
+++ zsh/functions/update.zsh
@@ -1,43 +1,43 @@
-function update_brew() {
+function update-brew() {
     brew update
     brew upgrade
     brew cleanup
     cd $DOTFILES/brew && brew bundle dump -f
 }
 
-function update_vim() {
+function update-vim() {
     nvim +PlugUpgrade +PlugUpdate +PlugClean! +qall >/dev/null
 }
 
-function update_tmux() {
+function update-tmux() {
     cd ~/.tmux/plugins/tpm/bin
     sh install_plugins
-    sh update_plugins all
+    sh update-plugins all
     sh clean_plugins
 }
 
-function update_pip() {
+function update-pip() {
     pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 }
 
-function update_venv_pip() {
+function update-venv_pip() {
     for venv_requirements_path in `find $PERSONAL/python/requirements/venvs/*`; do
         pyv up $(basename $venv_requirements_path)
-        update_pip
+        update-pip
         pip freeze > $venv_requirements_path
         deactivate
     done
 }
 
-function update_gcloud() {
+function update-gcloud() {
     gcloud components update
 }
 
-function update_all() {
-    update_brew
-    update_vim
-    update_tmux
-    update_pip
-    update_venv_pip
-    update_gcloud
+function update-all() {
+    update-brew
+    update-vim
+    update-tmux
+    update-pip
+    update-venv_pip
+    update-gcloud
 }
