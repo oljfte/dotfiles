324f130a2575ae8b11918e42aabf086dbec68593
oljfte <oljfte@gmail.com>
2020-12-29 20:13:39 +0900

    Fix wrong source path

--- zsh/_functions.zsh
+++ zsh/_functions.zsh
@@ -1,6 +1,6 @@
 # Dependencies
 source ${$(readlink $HOME/.zshrc)%/*}/__externals.zsh
 
-for function_source in `ls -1 $PERSONAL/zsh/functions/*`; do
+for function_source in `ls -1 $DOTFILES/zsh/functions/*`; do
     source $function_source;
 done
