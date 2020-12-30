e34f85c1442417d6fd88fdddc48f88cf94a61719
oljfte <oljfte@gmail.com>
2020-12-31 06:57:54 +0900

    Simplify rg related commannds using rg alias

--- zsh/externals/fzf.zsh
+++ zsh/externals/fzf.zsh
@@ -6,7 +6,7 @@ source $(brew --prefix)/opt/fzf/shell/key-bindings.zsh
 export PATH="$(brew --prefix)/opt/fzf/bin:$PATH"
 export FZF_TMUX=1
 export FZF_TMUX_OPTS='-p'
-export FZF_DEFAULT_COMMAND="rg --files --hidden --no-ignore --follow --glob '!.git/*' --glob '*.DS_Store*'"
+export FZF_DEFAULT_COMMAND="rg"
 export FZF_DEFAULT_OPTS="--reverse --inline-info --multi --bind tab:toggle,shift-tab:toggle-all,∆:down,˚:up,Ô:preview-down,:preview-up"
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'"
