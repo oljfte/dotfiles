38751ac40419d2c5620dd1f39ace5edd5456bd08
oljfte <oljfte@gmail.com>
2021-01-06 17:25:12 +0900

    Add custom snippets

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 [ ! -e ~/.config/nvim/colors ] && mkdir -p ~/.config/nvim/colors 
+[ ! -e ~/.config/nvim/snippets ] && mkdir -p ~/.config/nvim/snippets
 ln -fs $DOTFILES/nvim/init.vim ~/.config/nvim
 ln -fs $DOTFILES/nvim/coc-settings.json ~/.config/nvim
 ln -fs $DOTFILES/nvim/colors/* ~/.config/nvim/colors
+ln -fs $PERSONAL/nvim/snippets/* ~/.config/nvim/snippets
