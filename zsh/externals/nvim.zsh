12097c99c55e885f43ae262d4df978dd608718f2
oljfte <oljfte@gmail.com>
2021-01-06 17:27:42 +0900

    Fix order of lines in nvim.zsh

--- zsh/externals/nvim.zsh
+++ zsh/externals/nvim.zsh
@@ -2,10 +2,10 @@
 alias vi="vim"
 alias vim="nvim"
 
+# For ARM (may be temporary)
+alias nvim="VIMRUNTIME=$HOME/neovim/runtime $HOME/neovim/build/bin/nvim"
+
 # Functions
 function update-nvim() {
     nvim +PlugUpgrade +PlugUpdate +PlugClean! +qall >/dev/null
 }
-
-# For ARM (may be temporary)
-alias nvim="VIMRUNTIME=$HOME/neovim/runtime $HOME/neovim/build/bin/nvim"
