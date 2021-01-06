c5f1e27a7c80f6c24b32eafdf892f45933eccb24
oljfte <oljfte@gmail.com>
2021-01-06 17:28:15 +0900

    Change keybind for fzf

--- zsh/externals/fzf.zsh
+++ zsh/externals/fzf.zsh
@@ -6,8 +6,8 @@ source $(brew --prefix)/opt/fzf/shell/key-bindings.zsh
 export PATH="$(brew --prefix)/opt/fzf/bin:$PATH"
 export FZF_TMUX=1
 export FZF_TMUX_OPTS='-p 80%'
-export FZF_DEFAULT_COMMAND="rg --files --hidden --no-ignore --follow --glob '!**/.git/*' --glob '!*.DS_Store*'"
-export FZF_DEFAULT_OPTS="--reverse --color 16 --inline-info --multi --bind tab:toggle,shift-tab:toggle-all,∆:down,˚:up,Ô:preview-down,:preview-up"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --no-ignore --follow --glob '!**/.git/*' --glob '!*.DS_Store*' 2>/dev/null"
+export FZF_DEFAULT_OPTS="--reverse --color 16 --inline-info --multi --bind tab:toggle,shift-tab:toggle-all,ctrl-j:down,ctrl-k:up,ctrl-l:preview-down,ctrl-h:preview-up"
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_CTRL_T_OPTS="--preview 'bat {}'"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
