e9ffd7c5279d72cea646866aa4f4525e7ab355c7
oljfte <oljfte@gmail.com>
2021-01-09 05:23:16 +0900

    Use poetry

--- zsh/externals/python.zsh
+++ zsh/externals/python.zsh
@@ -1,50 +1,6 @@
 # Environmental variables
-export PYVENVS="$ENV_UTILS/local/python/venvs"
 export PYTHONDONTWRITEBYTECODE=1
 
 # Aliases
 alias python="python3"
 alias pip="pip3"
-
-# Functions
-function pyv() {
-    if [ "$1" = "create" ]; then
-        if [ ! -z "$2" ]; then
-            python -m venv $PYVENVS/$2 --system-site-packages
-            ln -fs $PERSONAL/python/sitecustomize.py $PYVENVS/$2/lib/python*/site-packages
-        else
-            echo "Specify venv name."
-        fi
-    elif [ "$1" = "list" ]; then
-        ls -A1 $PYVENVS
-    elif [ "$1" = "up" ]; then
-        if [ ! -z "$2" ]; then
-            source $PYVENVS/$2/bin/activate
-        else
-            venv_name=$(ls -A1 $PYVENVS | fzf)
-            [ ! -z "$venv_name" ] && source $PYVENVS/$venv_name/bin/activate
-        fi
-    elif [ "$1" = "delete" ]; then
-        if [ ! -z "$2" ]; then
-            trash $PYVENVS/$2 && echo "Deleted venv $2 successfully."
-        else
-            venv_name=$(ls -A1 $PYVENVS | fzf)
-            [ ! -z "$venv_name" ] && trash $PYVENVS/$venv_name && echo "Deleted venv $venv_name successfully."
-        fi
-    else
-        echo "Invalid arguments."
-    fi
-}
-
-function update-pip() {
-    pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-}
-
-function update-venv-pip() {
-    for venv_requirements_path in `find $personal/python/requirements/venvs/*`; do
-        pyv up $(basename $venv_requirements_path)
-        update-pip
-        pip freeze > $venv_requirements_path
-        deactivate
-    done
-}
