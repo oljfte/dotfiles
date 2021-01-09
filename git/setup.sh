ae605c651c30367beb488539862910a39b6c603f
oljfte <oljfte@gmail.com>
2021-01-09 23:42:44 +0900

    Dissociate personal stuff

--- git/setup.sh
+++ git/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/zsh
 
-command -v brew >/dev/null || zsh $DOTFILES/brew/setup.sh
-
-if [ `command -v gh >/dev/null` ]; then
-    gh config set git_protocol ssh && gh auth login --hostname github.com --with-token < $DOTFILES/git/gh/token.txt \
-        && rm $DOTFILES/git/gh/token.txt
-fi
-
 ln -fs $DOTFILES/git/.gitignore_global $HOME
--- setup_all.sh
+++ setup_all.sh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-# Receive GitHub token and check if it's valid
-if [ -z $GITHUB_TOKEN ]; then
-    while [[ $(curl https://api.github.com/ -u oljfte:$github_token -o /dev/null -w '%{http_code}\n' -s) -eq 200 ]]; do
-        echo "Paste GitHub personal access token:"
-        read github_token
-    done
-else
-    github_token=$GITHUB_TOKEN
-fi
-
 # Install Xcode
 xcode-select --install
 
@@ -21,12 +11,5 @@ export DOTFILES=${dotfiles_parent_dir}/dotfiles
 
 cd $dotfiles_parent_dir && git clone https://github.com/oljfte/dotfiles.git
 
-# Save github token to gh directory
-echo $github_token > $DOTFILES/git/gh/token.txt
-
 # Run setup scripts for global configs
 for setup_script in $DOTFILES/*/setup.sh; do zsh $setup_script; done
-
-# Run setup scripts for personal stuff
-[ ! -z $PERSONAL ] && [ -d $PERSONAL ] && zsh $PERSONAL/setup_all.sh
-
