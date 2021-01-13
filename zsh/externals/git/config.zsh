abd7629d53b74ebc36ff4ae6da582eee01e7cfa0
oljfte <oljfte@gmail.com>
2021-01-14 01:32:10 +0900

    Add git funcs/aliases

--- zsh/externals/git/config.zsh
+++ zsh/externals/git/config.zsh
@@ -1,6 +1,15 @@
 # Environment variables
 export GIT_EDITOR=$EDITOR
 
+# Aliases
+alias gst="git status"
+alias gdf="git diff"
+alias gdc="git diff --cached"
+alias ggr="git log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
+alias gft="git fetch"
+alias GPS="git push"
+alias GPF="git push -f"
+
 # Functions
 autoload -z $(dirname $0)/functions/* 
 
@@ -15,7 +24,7 @@ function _join-lines {
 }
 
 for c in hash file branch tag; do
-    eval "fg$c-widget() { local result=\$(fg$c | _join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "fg$c-widget() { local result=\$(_fg$c | _join-lines); zle reset-prompt; LBUFFER+=\$result }"
     eval "zle -N fg$c-widget"
     eval "bindkey '^]${c:0:1}' fg$c-widget"
 done
