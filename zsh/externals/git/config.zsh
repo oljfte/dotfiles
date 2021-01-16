17b60f5c5ae4f88893f416adb4b4fcbc586ce758
oljfte <oljfte@gmail.com>
2021-01-16 23:51:42 +0900

    Improve/clean up git functions

--- zsh/externals/git/config.zsh
+++ zsh/externals/git/config.zsh
@@ -2,16 +2,8 @@
 export GIT_EDITOR=$EDITOR
 
 # Aliases
-alias gst="git status"
-alias gdf="git diff"
-alias gdc="git diff --cached"
-alias ggr="git log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
 alias gft="git fetch"
 alias gcb="git branch --show-current"
-alias gps='[[ $(git diff @{upstream}) ]] && (git log origin/$(git branch --show-current)..$(git branch --show-current); \
-  echo "\n---\nAre you sure you want to push? (y/n): "; read -q && git push origin HEAD || echo "\nAbort.") || echo "Everything up-to-date."'
-alias gpf='[[ $(git diff @{upstream}) ]] && (git log origin/$(git branch --show-current)..$(git branch --show-current); \
-  echo "\n---\nAre you sure you want to FORCE-push? (y/n): "; read -q && git push origin HEAD -f || echo "\nAbort.") || echo "Everything up-to-date."'
 
 # Functions
 autoload -z $(dirname $0)/functions/* 
