5ebe2cafde7c985414aff9e7336eae8f6a8714d0
oljfte <oljfte@gmail.com>
2021-01-21 16:24:16 +0900

    Separate some alias settings

--- zsh/__aliases.zsh
+++ zsh/__aliases.zsh
@@ -2,10 +2,8 @@
 source $DOTFILES/zsh/__variables.zsh
 
 alias sudo="sudo "
-alias ll="rm .DS_Store &>/dev/null; exa -alh --git"
 alias mv="mv -i"
 alias psg="ps aux | grep"
-alias delds="fd -H '^\.DS_Store$' -tf -X rm"
 alias shutdown="sudo shutdown -h now"
 alias restart="sudo shutdown -r now"
 alias relogin="exec $SHELL -l"
