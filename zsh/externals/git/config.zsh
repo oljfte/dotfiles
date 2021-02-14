0c6a4a973607efcd4d910163b80d4484467a8529
oljfte <oljfte@gmail.com>
2021-02-14 22:10:53 +0900

    Add alias gtp to go to git repo root

--- zsh/externals/git/config.zsh
+++ zsh/externals/git/config.zsh
@@ -4,6 +4,7 @@ export GIT_EDITOR=$EDITOR
 # Aliases
 alias gft="git fetch"
 alias gcb="git branch --show-current"
+alias gtp="cd $(git rev-parse --show-toplevel)"
 
 # Functions
 autoload -z $(dirname $0)/functions/* 
