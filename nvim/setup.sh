7c69d7c6b64b815dd2ad701b5143c533258ac6bd
oljfte <oljfte@gmail.com>
2021-05-30 17:31:41 +0900

    Fix path format inconsistencies

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-[ ! -e ~/.config/nvim/colors ] && mkdir -p ~/.config/nvim/colors 
-[ ! -e ~/.config/nvim/snippets ] && mkdir -p ~/.config/nvim/snippets
-ln -fs $DOTFILES/nvim/init.vim ~/.config/nvim
-ln -fs $DOTFILES/nvim/coc-settings.json ~/.config/nvim
-ln -fs $DOTFILES/nvim/colors/* ~/.config/nvim/colors
+[ ! -e $HOME/.config/nvim/colors ] && mkdir -p $HOME/.config/nvim/colors
+[ ! -e $HOME/.config/nvim/snippets ] && mkdir -p $HOME/.config/nvim/snippets
+ln -fs $DOTFILES/nvim/init.vim $HOME/.config/nvim
+ln -fs $DOTFILES/nvim/coc-settings.json $HOME/.config/nvim
+ln -fs $DOTFILES/nvim/colors/* $HOME/.config/nvim/colors
 
 echo "Done nvim setup."
