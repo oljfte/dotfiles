f77c68b924a14aa38a30d786387fef318816eb17
oljfte <oljfte@gmail.com>
2021-07-17 00:43:58 +0900

    Remove some aliases

--- zsh/__aliases.zsh
+++ zsh/__aliases.zsh
@@ -1,14 +1,8 @@
 # Dependencies
 source $DOTFILES/zsh/__constants.zsh
 
-alias sudo="sudo "
-alias mv="mv -i"
-alias psg="ps aux | grep"
-alias delat="xattr -c"
 alias shutdown="sudo shutdown -h now"
 alias restart="sudo shutdown -r now"
-alias re="exec $SHELL -l"
 alias dt="cd $DESKTOP"
 alias docs="cd $DOCUMENTS"
 alias dots="cd $DOTFILES"
-alias pers="cd $PERSONAL"
