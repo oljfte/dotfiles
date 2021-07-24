6c26b9e77b49a9541f73832e66249dd76ff2828b
oljfte <oljfte@gmail.com>
2021-07-24 12:52:44 +0900

    Delete dead code

--- macos/setup.sh
+++ macos/setup.sh
@@ -4,9 +4,4 @@ while read row; do
     eval defaults write $row
 done < $DOTFILES/macos/macos_defaults
 
-ln -fs $DOTFILES/macos/apps/* /Applications
-
-[ ! -e $HOME/.config/Totonoe ] && mkdir -p $HOME/.config/Totonoe
-ln -fs $DOTFILES/macos/arrangement.xml $HOME/.config/Totonoe
-
 echo "Done macos setup."
