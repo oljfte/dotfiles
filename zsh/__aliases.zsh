ce04f71cdb2485085ecbd899ee7762ecd65ea7ef
oljfte <oljfte@gmail.com>
2021-07-31 01:26:31 +0900

    Generate alias automatically from environment variables

--- zsh/__aliases.zsh
+++ zsh/__aliases.zsh
@@ -3,6 +3,11 @@ source $DOTFILES/zsh/__constants.zsh
 
 alias shutdown="sudo shutdown -h now"
 alias restart="sudo shutdown -r now"
-alias dt="cd $DESKTOP"
-alias docs="cd $DOCUMENTS"
-alias dots="cd $DOTFILES"
+
+# Alias key string in lower-case to its value only if it is a valid path
+# for every environment key-value pairs.
+while IFS="=" read -A i; do
+  if [[ $i[1]:l =~ ^.*[a-z].*$ && $i[2] =~ ^\/[^:]*$ ]] && [ -e $i[2] ]; then
+    eval "alias $i[1]:l=\"cd $i[2]\""
+  fi
+done <<< $(env)
