db7169cd596998b33086002188fe38af96858348
oljfte <oljfte@gmail.com>
2021-07-30 21:37:45 +0900

    Use bindkey C-f to open lazygit

--- zsh/externals/lazygit/config.zsh
+++ zsh/externals/lazygit/config.zsh
@@ -1,2 +1,13 @@
-# Aliases
-alias g="lazygit"
+# ZLE
+function open-lazygit {
+  git rev-parse HEAD > /dev/null 2>&1 || return
+
+  # NOTE: Calling lazygit from zle causes syscall error
+  BUFFER="lazygit"
+  zle transient-line
+  zle reset-prompt
+}
+
+zle -N open-lazygit
+bindkey -M vicmd '^f' open-lazygit
+bindkey -M viins '^f' open-lazygit
