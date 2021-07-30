7ff2bcad963b2d2c6626a1f99ef46a954ed43257
oljfte <oljfte@gmail.com>
2021-07-31 01:40:05 +0900

    Add Karabiner-Elements configs

---
+++ karabiner/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# https://karabiner-elements.pqrs.org/docs/manual/misc/configuration-file-path/
+ln -fs $DOTFILES/karabiner/karabiner $HOME/.config
+
+echo "Done Karabiner-Elements setup."
