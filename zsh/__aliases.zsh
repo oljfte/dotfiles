a20dc7ecdbf1686def8f2bc80de3b89715bf2efc
oljfte <oljfte@gmail.com>
2021-08-06 01:27:11 +0900

    Add alias screensaver

--- zsh/__aliases.zsh
+++ zsh/__aliases.zsh
@@ -3,6 +3,7 @@ source $DOTFILES/zsh/__constants.zsh
 
 alias shutdown="sudo shutdown -h now"
 alias restart="sudo shutdown -r now"
+alias screensaver="open -b com.apple.ScreenSaver.Engine"
 
 # Alias key string in lower-case to its value only if it is a valid path
 # for every environment key-value pairs.
