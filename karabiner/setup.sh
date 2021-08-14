927a8f4ea72690a59e65b19abe96ed283157fb02
oljfte <oljfte@gmail.com>
2021-08-14 16:07:29 +0900

    Stop using karabiner elements

--- karabiner/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# https://karabiner-elements.pqrs.org/docs/manual/misc/configuration-file-path/
-ln -fs $DOTFILES/karabiner/karabiner $HOME/.config
-
-echo "Done Karabiner-Elements setup."
--- macos/setup.sh
+++ macos/setup.sh
@@ -4,6 +4,14 @@ while read row; do
     eval defaults write $row
 done < $DOTFILES/macos/macos_defaults
 
+# LaunchAgents
+[ ! -e $HOME/Library/LaunchAgents ] && mkdir -p $HOME/Library/LaunchAgents
+ln -f $DOTFILES/macos/LaunchAgents/* $HOME/Library/LaunchAgents
+
+# Key Modifications
+[ ! -e $HOME/Library/KeyBindings ] && mkdir -p $HOME/Library/KeyBindings
+ln -f $DOTFILES/macos/DefaultKeyBinding.dict $HOME/Library/KeyBindings
+
 # Misc
 sudo systemsetup -setdisplaysleep "Never"
 
