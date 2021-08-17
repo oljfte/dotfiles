bfbe4ee86f960ffa1c8e82f5c0b02fe3713338d0
oljfte <oljfte@gmail.com>
2021-08-18 01:31:24 +0900

    Use ZLE keybinds to open tmux tools

--- zsh/externals/tmux/config.zsh
+++ zsh/externals/tmux/config.zsh
@@ -1,2 +1,11 @@
 # Functions
 autoload -z $(dirname $0)/functions/*
+
+# ZLE
+zle -N tm
+bindkey -M vicmd '^o' tm
+bindkey -M viins '^o' tm
+
+zle -N tp
+bindkey -M vicmd '^p' tp
+bindkey -M viins '^p' tp
