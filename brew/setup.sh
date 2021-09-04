af6fe808ddb339961cc12fb5839a003df0d81d64
oljfte <oljfte@gmail.com>
2021-09-05 01:10:49 +0900

    Make brew setup script idempotent

--- brew/setup.sh
+++ brew/setup.sh
@@ -4,6 +4,6 @@ if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew bundle --file "$DOTFILES/brew/Brewfile"
+brew bundle install --file "$DOTFILES/brew/Brewfile"
 
 echo "Done brew setup."
