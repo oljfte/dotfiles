b05a0eefdca13ccc6ce6931529bdf97a1adb7767
oljfte <oljfte@gmail.com>
2021-09-05 17:08:57 +0900

    Remove terminal profiles

--- macos/setup.sh
+++ macos/setup.sh
@@ -12,14 +12,6 @@ ln -f $DOTFILES/macos/LaunchAgents/* $HOME/Library/LaunchAgents
 [ ! -e $HOME/Library/KeyBindings ] && mkdir -p $HOME/Library/KeyBindings
 ln -f $DOTFILES/macos/DefaultKeyBinding.dict $HOME/Library/KeyBindings
 
-# Terminal
-terminal_profile_dir=$DOTFILES/macos/terminal_profiles
-terminal_profile_name=`basename $(ls -rt $terminal_profile_dir/*.terminal | tail -n 1) | cut -f 1 -d '.'`
-terminal_profile=$(<$terminal_profile_dir/$terminal_profile_name.terminal)
-plutil -insert Window\ Settings.$terminal_profile_name -xml "$terminal_profile" ~/Library/Preferences/com.apple.Terminal.plist
-defaults write com.apple.Terminal "Default Window Settings" -string "$terminal_profile_name"
-defaults write com.apple.Terminal "Startup Window Settings" -string "$terminal_profile_name"
-
 # Misc
 sudo systemsetup -setdisplaysleep "Never"
 
