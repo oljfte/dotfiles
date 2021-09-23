183515350a8fcb74e054de141c96d421a503b410
oljfte <oljfte@gmail.com>
2021-09-24 03:42:18 +0900

    Switch configs by displays

---
+++ app/phoenix/layout/laptop.js
@@ -0,0 +1,48 @@
+const WINDOW_LAYOUT = [
+  // Space 1
+  {
+    name: "Spotify",
+    gridFrame: { x: 0, y: 0, width: 7, height: 12 },
+    spaceIndex: 1,
+  },
+  {
+    name: "TweetDeck",
+    gridFrame: { x: 7, y: 0, width: 5, height: 12 },
+    spaceIndex: 1,
+  },
+
+  // Space 2
+  {
+    name: "Calendar",
+    gridFrame: { x: 0, y: 0, width: 12, height: 12 },
+    spaceIndex: 2,
+  },
+
+  // Space 3
+  {
+    name: "Mail",
+    gridFrame: { x: 0, y: 0, width: 12, height: 12 },
+    spaceIndex: 3,
+  },
+
+  // Space 4
+  {
+    name: "Slack",
+    gridFrame: { x: 0, y: 0, width: 12, height: 12 },
+    spaceIndex: 4,
+  },
+
+  // Space 5
+  {
+    name: "Safari",
+    gridFrame: { x: 0, y: 0, width: 12, height: 12 },
+    spaceIndex: 5,
+  },
+
+  // Space 6
+  {
+    name: "Term",
+    gridFrame: { x: 0, y: 0, width: 12, height: 12 },
+    spaceIndex: 6,
+  },
+];
--- app/phoenix/setup.sh
+++ app/phoenix/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 [ ! -e $HOME/.config/phoenix ] && mkdir -p $HOME/.config/phoenix
-ln -f $DOTFILES/app/phoenix/config/*.js $HOME/.config/phoenix
+ln -fs $DOTFILES/app/phoenix/config/*.js $HOME/.config/phoenix
+
+# Use layout for wide screen as default
+ln -fs $DOTFILES/app/phoenix/layout/home.js ~/.config/phoenix/layout.js
 
 echo "Done phoenix setup."
