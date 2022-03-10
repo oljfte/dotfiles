523173cdba921ca4b11c67b76c99b94d20540771
oljfte <oljfte@gmail.com>
2022-03-11 00:44:05 +0900

    Stop treating myself like a kid

--- cli/nvim/lua/keymaps.lua
+++ cli/nvim/lua/keymaps.lua
@@ -1,18 +1,5 @@
 local utils = require('utils')
 
-utils.keymap("i", "<Up>", "<Nop>")
-utils.keymap("i", "<Down>", "<Nop>")
-utils.keymap("i", "<Left>", "<Nop>")
-utils.keymap("i", "<Right>", "<Nop>")
-utils.keymap("n", "<Up>", "<Nop>")
-utils.keymap("n", "<Down>", "<Nop>")
-utils.keymap("n", "<Left>", "<Nop>")
-utils.keymap("n", "<Right>", "<Nop>")
-utils.keymap("v", "<Up>", "<Nop>")
-utils.keymap("v", "<Down>", "<Nop>")
-utils.keymap("v", "<Left>", "<Nop>")
-utils.keymap("v", "<Right>", "<Nop>")
-
 utils.keymap("n", "<ESC><ESC>", ":noh<CR>")
 
 utils.keymap("n", "n", "nzzzv")
