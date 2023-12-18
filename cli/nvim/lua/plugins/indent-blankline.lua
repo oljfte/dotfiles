7995bdad54899530407984dd63bd14b8641180f1
oljfte <oljfte@gmail.com>
2023-12-18 23:27:08 +0900

    Update indent-blankline config to follow version 3

--- cli/nvim/lua/plugins/indent-blankline.lua
+++ cli/nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,9 @@
 return function()
 	local utils = require("utils")
 	utils.highlight("IndentBlanklineChar", "#323236", "NONE", nil)
-	require("indent_blankline").setup({
-		char = "▏",
+	require("ibl").setup({
+		indent = {
+			char = "▏",
+		},
 	})
 end
