e822ad5f8481f06accda975688d7a0584c80f020
oljfte <oljfte@gmail.com>
2023-12-19 01:39:21 +0900

    Ensure all available parsers installed for treesitter

--- cli/nvim/lua/plugins/nvim-treesitter.lua
+++ cli/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,6 @@ return function()
 		},
 		indent = { enable = true },
 		autopairs = { enable = true },
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+		ensure_installed = "all",
 	})
 end
